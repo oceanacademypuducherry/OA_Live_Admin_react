94f4f79a6098e934a46e6a64e1d9d7183e5f941b
THAMIZH <thamizharasan2373@gmail.com>
2022-05-27 16:51:00 +0530

    webiar add and webinar mentor add completed

--- src/Components/Wbinar/AddWebinar/AddWebinar.jsx
+++ src/Components/Wbinar/AddWebinar/AddWebinar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextInput from "../../common/TextInput";
 import "./add_webinar.scss";
 import webinarVideo from "./webinar.mp4";
@@ -12,6 +12,7 @@ export default function AddWebinar() {
     bannerImage: "upload image",
     promoVideo: "upload video",
   });
+  const [avilableMentor, setAvilabelMentor] = useState([]);
   const [webinarInfo, setWbinarInfo] = useState({
     title: "",
     subtitle: "",
@@ -22,21 +23,24 @@ export default function AddWebinar() {
     isFree: true,
     isComplete: false,
     price: 0,
-    topics: [{ title: "", subtitle: "" }],
-    startAt: "2022-06-20T23:45:00.000+00:00",
+    topics: [{ title: "ti", subtitle: "sub" }],
+    startAt: "",
     mentor: "",
     promoVideo: "",
+    token: localStorage.getItem("a_token"),
   });
 
   function inputHandler(e) {
     const { value, name, type, files } = e.target;
-
-    if (type === "file") {
+    if (name === "topics") {
+      topicPicker(files[0]);
+    } else if (type === "file") {
       if (files[0] !== undefined) {
         console.log(files[0].type);
         imageUpload(files[0], name);
       }
     } else if (type === "datetime-local") {
+      console.log(value);
       let date = new Date(value).toISOString();
       console.log(date);
       setWbinarInfo({ ...webinarInfo, [name]: date });
@@ -76,6 +80,60 @@ export default function AddWebinar() {
     fileInp.style.display = "none";
     fileInp.click();
   }
+
+  function dateConverter() {
+    if (webinarInfo.startAt === "") {
+      return "DD-MM-YY HH:MM AM";
+    }
+    let fullDate = new Date(webinarInfo.startAt);
+    let hours = fullDate.getHours() === 12 ? 12 : fullDate.getHours() % 12;
+    return `${fullDate.getDate()}-${fullDate.getMonth()}-${fullDate.getFullYear()} ${hours}:${fullDate.getMinutes()} ${
+      fullDate.getHours() <= 11 || fullDate.getHours() === 0 ? "AM" : "PM"
+    }`;
+  }
+
+  async function topicPicker(pickedFile) {
+    var reader = new FileReader();
+    reader.readAsText(pickedFile, "UTF-8");
+    try {
+      reader.onload = (readerEvent) => {
+        var content = readerEvent.target.result;
+        const topics = JSON.parse(content);
+        if (topics[0].subtitle === undefined || topics[0].title === undefined) {
+          alert("invalid syllabus");
+        } else {
+          console.log(topics);
+          setWbinarInfo({ ...webinarInfo, topics: topics });
+        }
+      };
+    } catch (e) {
+      console.log(e.message);
+      alert("invalid syllabus");
+    }
+  }
+  function getAllWebinarMentor() {
+    axios
+      .post("/webinar/mentor/all", { token: localStorage.getItem("a_token") })
+      .then((res) => {
+        setAvilabelMentor(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  function addWebinar() {
+    axios
+      .post("/webinar/add", webinarInfo)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  useEffect(() => {
+    getAllWebinarMentor();
+  }, []);
   return (
     <div className="add-webinar-div">
       <div className="webinar-form-div">
@@ -157,54 +215,65 @@ export default function AddWebinar() {
           />
           <div className="webinar-inp date-picker">
             <input
+              id="date-pi"
               type="datetime-local"
               name="startAt"
               value={webinarInfo.startAt}
               onChange={inputHandler}
             />
+            <div className="date-time">{dateConverter()}</div>
           </div>
           <div className="webinar-inp mentor-picker">
             <select
               name="mentor"
               // value={webinarInfo.mentor}
               onChange={inputHandler}
+              // style={{ height: 40 }}
             >
-              <option>Thamizh</option>
-              <option>Ijass</option>
+              <option value="">Select</option>
+              {avilableMentor.map((mentor, index) => {
+                return (
+                  <option key={index} value={mentor._id}>
+                    {mentor.mentorName}
+                  </option>
+                );
+              })}
             </select>
           </div>
-          <button className="status-btn">Not Completed</button>
+          <button
+            className="status-btn"
+            style={{ background: webinarInfo.isComplete ? "green" : "red" }}
+            onClick={() => {
+              setWbinarInfo({
+                ...webinarInfo,
+                isComplete: !webinarInfo.isComplete,
+              });
+            }}
+          >
+            {webinarInfo.isComplete ? "Completed" : "Not Completed"}
+          </button>
         </div>
       </div>
-      <div className="topic-div">
+      <div className="topic-div" onClick={() => filePick("topics-json")}>
+        <input
+          type="hidden"
+          className="topics-json"
+          name="topics"
+          onChange={inputHandler}
+        />
         <div className="topic-title">Topic</div>
         <div className="topics">
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
-          <span className="topic">Intro</span>
+          {webinarInfo.topics.map((item, index) => (
+            <span key={index} className="topic">
+              {item.title}
+            </span>
+          ))}
         </div>
       </div>
       <div
         className="c-btn addmentor"
         style={{ width: 300 }}
-        onClick={() => {
-          console.log(webinarInfo);
-        }}
+        onClick={addWebinar}
       >
         Add Mentor
       </div>
